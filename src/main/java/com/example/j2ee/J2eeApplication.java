d0ad13c041659e56c86b01d3d25ee0375f077060
maple826 <zhangyichi0826@gmail.com>
2023-06-02 23:49:28 +0800

    添加了启动类的注解

--- src/main/java/com/example/j2ee/J2eeApplication.java
+++ src/main/java/com/example/j2ee/J2eeApplication.java
@@ -1,8 +1,12 @@
 package com.example.j2ee;
 
+import com.github.xiaoymin.swaggerbootstrapui.annotations.EnableSwaggerBootstrapUI;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableSwaggerBootstrapUI
+@EnableJpaRepositories
 @SpringBootApplication
 public class J2eeApplication {
 
